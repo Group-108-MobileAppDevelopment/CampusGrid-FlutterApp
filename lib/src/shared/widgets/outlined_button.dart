af72e6e3cb0a62c19cc4e15dff26582dbbf08df0
HadeedTariq <hadeedtariq50@gmail.com>
2026-01-01 12:55:26 +0500

    Added the outlined button

---
+++ lib/src/shared/widgets/outlined_button.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CustomOutlinedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+  final IconData? leadingIcon;
+  final IconData? trailingIcon;
+  final bool isLoading;
+  final Color? buttonColor;
+
+  const CustomOutlinedButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.leadingIcon,
+    this.trailingIcon,
+    this.isLoading = false,
+    this.buttonColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: OutlinedButton(
+        onPressed: isLoading ? null : onPressed,
+        style: OutlinedButton.styleFrom(
+          side: BorderSide(color: buttonColor ?? Theme.of(context).colorScheme.primary), 
+          foregroundColor: buttonColor ?? Theme.of(context).colorScheme.primary,
+        ),
+        child: isLoading
+            ? SizedBox(
+                height: 24,
+                width: 24,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2.5,
+                  valueColor: AlwaysStoppedAnimation(
+                    buttonColor ?? Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (leadingIcon != null) ...[
+                    Icon(leadingIcon, size: 22),
+                    const SizedBox(width: 12),
+                  ],
+                  Text(text),
+                  if (trailingIcon != null) ...[
+                    const SizedBox(width: 4),
+                    Icon(trailingIcon, size: 24),
+                  ],
+                ],
+              ),
+      ),
+    );
+  }
+}
