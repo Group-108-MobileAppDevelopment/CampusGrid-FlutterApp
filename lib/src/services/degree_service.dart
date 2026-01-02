55dd4ced28dd279d34020571968e82cbc4c57f84
HadeedTariq <hadeedtariq50@gmail.com>
2026-01-02 22:18:10 +0500

    Added Degree level service

---
+++ lib/src/services/degree_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+Future<List<Map<String, dynamic>>> getDegreesByDepartment(String depId) async {
+  final snapshot = await _firestore
+      .collection('degree')
+      .where('depId', isEqualTo: depId)
+      .get();
+  return snapshot.docs.map((doc) {
+    return {
+      'id': doc.id,
+      ...doc.data(),
+    };
+  }).toList();
+}
+
+Future<Map<String, dynamic>> getDegreeById(String degId) async {
+  final doc = await _firestore.collection('degree').doc(degId).get();
+  if (doc.exists) {
+    return {
+      'id': doc.id,
+      ...doc.data()!,
+    };
+  } else {
+    throw Exception('Degree not found.');
+  }
+}
