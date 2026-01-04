fdb828bce145072d625737c71dcab2e9712c6df0
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-04 22:32:31 +0500

    fix: modify firebase_options.dart for web

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -59,4 +56,14 @@ class DefaultFirebaseOptions {
     projectId: 'campus-grid-95dd6',
     storageBucket: 'campus-grid-95dd6.firebasestorage.app',
   );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBVSvqUaCMhMMHkuTjsNTY0mMOCOSuKDd0',
+    appId: '1:786486152562:web:0f909c7fbd0508f446e1eb',
+    messagingSenderId: '786486152562',
+    projectId: 'campus-grid-95dd6',
+    authDomain: 'campus-grid-95dd6.firebaseapp.com',
+    storageBucket: 'campus-grid-95dd6.firebasestorage.app',
+  );
+
 }
